584c7cfaf203154c4721035f4c0909924202ad83
Greg Van Aken <gavanaken@gmail.com>
2017-12-18 12:58:55 -0500

    Made some edits towards the final project -- commiting now just in case.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,43 @@ words["<"] = lessThan;
 words[">"] = greaterThan;
 words["="] = equal;
 words[".s"] = dotS;
+words["dup"] = dup;
 
+// Stack Class
+function Stack () {
+    this.elements = [];
+}
+
+Stack.prototype.pop = function() { return this.elements.pop();};
+Stack.prototype.push = function(item) {this.elements.push(item);};
+Stack.prototype.getElements = function() {return this.elements;};
+
+// ObservableStack class
+function ObservableStack() {
+    Stack.apply(this, arguments);
+    this.observers = [render];
+}
+
+ObservableStack.prototype = new Stack();
+ObservableStack.prototype.pop = function() {
+    a = this.elements.pop();
+    this.registerObserver(); 
+    return a;
+};
+ObservableStack.prototype.push = function(item) {
+    this.elements.push(item);
+    this.registerObserver();
+};
+ObservableStack.prototype.subscribe = function(fn) {this.observers.push(fn);};
+ObservableStack.prototype.registerObserver = function() {
+    for (var i = 0; i < this.observers.length; i++) {
+       this.observers[i](this);
+    }
+}
+
+function render(stack){
+    renderStack(stack);
+}
 
 /**
  * Print a string out to the terminal, and update its scroll to the
@@ -33,13 +69,13 @@ function print(terminal, msg) {
  */
 function renderStack(stack) {
     $("#thestack").empty();
-    stack.slice().reverse().forEach(function(element) {
+    stack.getElements().slice().reverse().forEach(function(element) {
         $("#thestack").append("<tr><td>" + element + "</td></tr>");
     });
 };
 
 function dotS(stack, terminal) {
-    print(terminal, " <" + stack.length + "> " + stack.slice().join(" "));
+    print(terminal, " <" + stack.getElements().length + "> " + stack.getElements().slice().join(" "));
     return;
 }
 
@@ -53,7 +89,7 @@ function add(stack, terminal) {
 function sub(stack, terminal) {
     var first = stack.pop();
     var second = stack.pop();
-    stack.push(first-second);
+    stack.push(second-first);
     return;
 }
 
@@ -89,6 +125,13 @@ function dup(stack, terminal) {
 function nip(stack, terminal) {
     swap(stack);
     stack.pop();
+    return;
+}
+
+function dup(stack, terminal) {
+    var a = stack.pop();
+    stack.push(a);
+    stack.push(a);
     return;
 }
 
@@ -151,19 +194,78 @@ function process(stack, input, terminal) {
     if (!(isNaN(Number(input)))) {
         print(terminal,"pushing " + Number(input));
         stack.push(Number(input));
-    } else if (input in words) {
+    } 
+    else if (input in words) {
         if (words[input] instanceof Array) {
-            for (cmd in words[input]) {
-                process(stack, words[input][cmd], terminal)
+            var commands = words[input].slice();
+            var cmd = 0;
+            while (cmd < commands.length) {
+                if (commands[cmd] === "if") {
+                    var ifplace = cmd;
+                    var numif = 1;
+                    var numelse = 0;
+                    cmd++;                    
+                    var ifcmd = [];
+                    var elsecmd = [];
+                    while (numif != numelse) {
+                        if (commands[cmd] === "if") {
+                            numif++;
+                        }
+                        if (commands[cmd] === "else") {
+                            numelse++;
+                        }
+                        ifcmd.push(commands[cmd]);
+                        cmd++;
+                    }
+                    ifcmd.pop() // get rid of that else that was pushed
+                    while (commands[cmd] != "end") {
+                        elsecmd.push(commands[cmd]);
+                        cmd++;
+                    }
+                    var endplace = cmd;
+                    cmd++;
+                    var top = stack.pop();
+                    if (top === 0) {
+                        words[input+"else"] = elsecmd;
+                        process(stack, input+"else", terminal);
+                    }
+                    else {
+                        words[input+"if"] = ifcmd;
+                        process(stack, input+"if", terminal)
+                    }
+                    var beforeif = commands.slice(ifplace);
+                    var afterend = commands.slice(endplace, words[input].length);
+                    commands = beforeif.concat(afterend);
+                }
+                else {
+                process(stack, words[input][cmd], terminal);
+                }
+                cmd++;
             }
-        } else {
-        words[input](stack, terminal);
         }
-    } else {
-        print(terminal, ":-( Unrecognized input");
+        else {
+            words[input](stack, terminal);
+        }
     }
-    renderStack(stack);
+    else {
+        print(terminal, ":-( Unrecognized input: " + String(input));
+    }
+    //renderStack(stack);
 };
+
+function processif(stack, input, terminal) {
+    var test = stack.pop();
+    if (test === -1) {
+        for (cmd in words[input+"if"]) {
+            process(stack, words[input+"if"][cmd], terminal);
+        }
+    }
+    else {
+        for (cmd in words[input+"else"]) {
+            process(stack, words[input+"else"][cmd], terminal);
+        }
+    }
+}
 
 
 function runRepl(terminal, stack) {
@@ -179,10 +281,22 @@ function runRepl(terminal, stack) {
                 var funckey = commands[i];
                 i++;
                 while(commands[i] != ";") {
-                    UDfunc.push(commands[i]);  // still need to put this somewhere and process them
+                    UDfunc.push(commands[i]);  
                     i++;
-                }
+                    }
                 words[funckey] = UDfunc;
+                var div = document.getElementById('buttons go here');
+                var btn = document.createElement('button');
+                var txt = document.createTextNode(funckey);
+
+                btn.appendChild(txt);
+                btn.setAttribute('type', 'button');
+                btn.setAttribute('id', 'button ' + funckey)
+                btn.onclick = function execute() {
+                    process(stack, btn.childNodes[0].textContent, terminal)
+                };
+                div.appendChild(btn);
+
             }
             else {
                 process(stack, commands[i], terminal);
@@ -204,7 +318,7 @@ $(document).ready(function() {
     // represents the terminal to the end of it.
     $("#terminal").append(terminal.html);
 
-    var stack = [];
+    var stack = new ObservableStack;
 
     print(terminal, "Welcome to HaverForth! v0.1");
     print(terminal, "As you type, the stack (on the right) will be kept in sync");
@@ -214,10 +328,11 @@ $(document).ready(function() {
     var resetButton = $("#reset");
     resetButton.click(function emptyStack() {
     // Clearing the stack
-    while (stack.length != 0) {
+    while (stack.getElements().length != 0) {
         stack.pop();
     }
     renderStack(stack);
 });
+
     runRepl(terminal, stack);
 });
